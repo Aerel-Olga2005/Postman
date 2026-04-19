98cd9b4231b2bb8fd51f67a56fe66b83df2dee9b
Helga NN <o9351030@gmail.com>
2026-04-19 19:12:22 +0300

    correct data

--- src/test/java/ru/netology/rest/PostmanTest.java
+++ src/test/java/ru/netology/rest/PostmanTest.java
@@ -19,7 +19,7 @@ public class PostmanTest {
                 .then()
                 .log().all()
                 .statusCode(200)
-                .body("data", org.hamcrest.Matchers.equalTo("Invierno"))
+                .body("data", org.hamcrest.Matchers.equalTo("Primavera"))
         ;
     }
 }
