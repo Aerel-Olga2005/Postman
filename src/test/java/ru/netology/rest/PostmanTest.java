db9478dc6f87c22581ee44d48e2c111bc9bdbf10
Helga NN <o9351030@gmail.com>
2026-04-19 18:45:32 +0300

    Initial commit

---
+++ src/test/java/ru/netology/rest/PostmanTest.java
@@ -0,0 +1,25 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+public class PostmanTest {
+    @Test
+    void ShouldReturnSendData() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Primavera") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .log().all()
+                .post("/post")
+// Проверки
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("Primavera"))
+        ;
+    }
+}
